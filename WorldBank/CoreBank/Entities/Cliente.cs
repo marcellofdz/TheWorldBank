9cdf198bc613c0b57614c51be500b4b6b954ebcc
Diego Lobato <dlobato@ofimatic.com>
2021-04-19 11:22:23 -0400

    Agregando las clases de clientes y empleados

---
+++ WorldBank/CoreBank/Entities/Cliente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreBank.Entities
+{
+    public class Cliente
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Cedula { get; set; }
+
+        public string Telefono { get; set; }
+
+        public string Direccion { get; set; }
+
+
+    }
+}
---
+++ WorldBank/CoreBank/Entities/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreBank.Entities
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        public int StatusId { get; set; }
+    }
+}
