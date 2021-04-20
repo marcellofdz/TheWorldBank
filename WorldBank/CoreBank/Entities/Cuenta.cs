b7f152f0a218a2c4ac2ec73c8cd19434a6ac45d1
IvyMasterDiego <diegolobato82@gmail.com>
2021-04-20 00:33:53 -0400

    Agregando nuevos campos para los distintos procesos del core

---
+++ WorldBank/CoreBank/Entities/Cuenta.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreBank.Entities
+{
+    public class Cuenta
+    {
+        public int Id { get; set; }
+
+        public string Number { get; set; }
+
+        public Cliente Nombre { get; set; }
+
+        public int NombreId { get; set; }
+
+        pub
+    }
+}
